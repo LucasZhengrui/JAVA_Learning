1b31b184dd589cafb584a1c33931178a7fad86a9
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-09-23 09:17:28 +0800

    Create Day_switch.java

---
+++ Third_class/Day_switch.java
@@ -0,0 +1,37 @@
+package while_for_dowhile;
+
+import java.util.Scanner;
+
+public class Day_print {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner day = new Scanner(System.in);
+		int n;
+		n = day.nextInt();
+		switch(n)
+		{
+			case 1: System.out.println("Monday");
+			break;
+			case 2: System.out.println("Tuesday");
+			break;
+			case 3: System.out.println("Wednesday");
+			break;
+			case 4: System.out.println("Thursday");
+			break;
+			case 5: System.out.println("Friday");
+			break;
+			case 6: System.out.println("Saturday");
+			break;
+			case 7: System.out.println("Sunday");
+			break;
+			default: System.out.println("Inout Error");
+			break;
+		}
+	}
+
+}
+
+
+// 7
+// Sunday
