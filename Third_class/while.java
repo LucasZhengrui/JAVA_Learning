a5917911d872f55a4d9da2a538545ab3aabe48dc
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-09-23 08:46:47 +0800

    Create while.java

---
+++ Third_class/while.java
@@ -0,0 +1,20 @@
+package while_for_dowhile;
+
+public class while_learning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		int i=100;
+		while(i<200)
+		{
+			sum+=i;
+			i++;
+		}
+		System.out.println("The sum is " +sum);
+	}
+
+}
+
+
+//The sum is 14950
