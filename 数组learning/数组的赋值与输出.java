ee482fd689e4037d66781cebfbd8e8c53d1baf63
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-11-09 11:11:00 +0000

    Create 数组的赋值与输出.java

---
+++ 数组learning/数组的赋值与输出.java
@@ -0,0 +1,21 @@
+package Java_code;
+
+public class Java_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] anArray;	          // 声明一个整型数组
+	     //*********Found********
+	        anArray = new int[10] ;    // 创建数组,包含10个元素。（此处为填空）
+	      
+	        // 给数组每个元素赋值并打印输出
+	        for (int i = 0; i < anArray.length; i++)
+	        {
+	     //*********Found********
+	            anArray[i]=i;// 此处为填空
+	            System.out.print(anArray[i] + " ");
+	        }
+	        System.out.println();
+	}
+
+}
