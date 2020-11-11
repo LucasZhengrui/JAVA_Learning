96f7c4b4248593c44b9a09516bd6cf6e3b76afc9
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-11-11 09:13:49 +0800

    Create 3.java

---
+++ ((x>x)?x:x)/3.java
@@ -0,0 +1,12 @@
+
+public class Conditional {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=4;
+		System.out.println("value is "+((x>4)?99.9:9));
+	}
+
+}
+
+// value is 9.0
