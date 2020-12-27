910fec0db61323f06abde6bd242c505057a278ee
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 13:03:20 +0000

    Create code.java

---
+++ 题库/试卷18/3/code.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Java_3 extends JApplet
+{
+     //*********Found********
+   public void _____()
+   {
+      Container contentPane = getContentPane();
+     //*********Found********
+      JLabel label = new _____
+         ("Java是一个跨平台的面向网络的计算机语言。",
+         SwingConstants.CENTER);
+     //*********Found********
+      contentPane.add(_____);
+   }
+   
+}
