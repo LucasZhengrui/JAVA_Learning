b8ebc321874d2b58566caba7e71d621da3f7d4b5
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:36:35 +0000

    Create code.java

---
+++ 题库/试卷6/1/code.java
@@ -0,0 +1,37 @@
+// 本题的要求是∶ 
+// 完成程序并运行求得答案。 
+// 运行结果为∶ 
+// TQ_IN= 120.67 
+// b= 39 
+// i= 4096 
+// c= 9 
+// d= 5.68889 
+// 【第一空】 参考答案∶ class
+// 【第二空】 参考答案∶char 
+
+import java.io.*;
+
+     //*********Found********
+public _________ Java_1
+{
+    public static void main(String args[])
+    {
+        final  double TQ_IN=120.67;
+
+        byte   b;
+        int    i;
+     //*********Found********
+        ______ c;
+        double d=5.68889;
+
+        b=047;
+        i=4096;
+        c='9';
+
+        System.out.println("TQ_IN= "+TQ_IN);
+        System.out.println("b= "+b);
+        System.out.println("i= "+i);
+        System.out.println("c= "+c);
+        System.out.println("d= "+d);
+    }
+}
