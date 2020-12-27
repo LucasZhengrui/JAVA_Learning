524501b0eb70b45000f9c4c16045d237d8d4a8a1
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 11:35:37 +0000

    Create code.java

---
+++ 题库/试卷9/1/code.java
@@ -0,0 +1,22 @@
+// 本题的要求是∶
+// 完成程序并运行得结果为∶
+// Abstract
+// Window
+// Tool kit
+// 【第一空】参考答案∶ String
+// 【第二空】参考答案∶ awt[i]
+
+public class Java_1{
+   public static void main(String[] args){
+
+     //*********Found********
+      String[] awt = new ___________[3];
+      awt[0] = "Abstract";
+      awt[1] = "Window";
+      awt[2] = "Tool kit";
+
+      for (int i = 0; i < 3; i++)
+     //*********Found********
+         System.out.println(___________);
+   }
+}
