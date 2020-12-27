42e7e23a78228bc4fa8604aeb1dd102e7013e350
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 11:37:55 +0000

    Create code.java

---
+++ 题库/试卷9/3/code.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import javax.swing.*;
+
+//*********Found**********
+public class Java_3 __________________ JApplet{
+//*********Found**********
+  public void ___________________ (){
+    Container contentPane = getContentPane();
+    JLabel label = new JLabel("Java的诞生是对传统计算模式的挑战！",
+         SwingConstants.CENTER);
+//*********Found**********
+    contentPane.add(_____________________);
+  }
+}
