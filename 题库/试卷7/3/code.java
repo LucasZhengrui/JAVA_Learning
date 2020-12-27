f3b68eeca556d3dd8276de1910984760c5dc66ed
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:43:46 +0000

    Create code.java

---
+++ 题库/试卷7/3/code.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Java_3
+{
+    public static void main(String[] args)
+    {
+        WelcomFrame frame = new WelcomFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     //*********Found********
+        frame.setVisible(____________________);
+    }
+}
+
+     //*********Found********
+class WelcomFrame ____________ JFrame
+{
+    public WelcomFrame()
+    {
+        setTitle("Java等级考试");
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+        WelcomPanel panel = new WelcomPanel();
+        Container contentPane = getContentPane();
+        contentPane.add(panel);
+    }
+    public static final int DEFAULT_WIDTH = 250;
+    public static final int DEFAULT_HEIGHT = 100;
+}
+
+     //*********Found********
+class WelcomPanel extends ____________       
+{
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        g.drawString("欢迎参加Java等级考试！", MESSAGE_X, MESSAGE_Y);
+        setBackground(Color.white);
+    }
+    public static final int MESSAGE_X = 60;
+    public static final int MESSAGE_Y = 50;
+}
