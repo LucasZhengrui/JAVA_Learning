d9da42d014db4ba11f6c0da5646dbfadc075fc72
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 12:08:51 +0000

    Update code.java

--- 题库/试卷14/2/code.java
+++ 题库/试卷14/2/code.java
@@ -1,3 +1,10 @@
+// 本题的要求是∶ 
+// Java_2.java程序的功能是在一个整型数组中查找指定的数，找到后将该数 
+// 字及其位置打印出来。完成后并运行，所得结果为：
+// Found 12 at index 4 
+// 【第一空】 参考答案∶arrayOfInts.length 
+// 【第二空】 参考答案∶true 
+
 public class Java_2 {
    public static void main(String[] args) {
       int[] arrayOfInts = { 32, 87, 3, 589, 12, 1076, 2000, 8, 622, 127 };
