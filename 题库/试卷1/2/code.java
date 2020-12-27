ca4aefbf218f48673a26c5708ad10c2ccf7944b5
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:13:24 +0000

    Update code.java

--- 题库/试卷1/2/code.java
+++ 题库/试卷1/2/code.java
@@ -1,3 +1,10 @@
+// 本题的要求是∶ 
+// 打开一个文本文件test.txt，统计该文件中包含的数字的个数与英文字母的个数（忽略大写）。例如itest.txt文本文件中包含的内容为"123abcDEF99G"，则程序的输出结果如下; 
+// 数字数是5，字母数是7 
+// 【第一空】 参考答案; try 
+// 【第二空】 参考答案∶new FileInputStream
+
+
 import java.io.*;
 public class Java_2{
    public static void main (String[] args){
@@ -22,6 +29,3 @@ public class Java_2{
       System.out.println("数字数是 " + c1 + "，字母数是 " + c2);
    }
 }
-
-// 【第一空】 参考答案; try 
-// 【第二空】 参考答案∶new FileInputStream
