217f740108eb705feb70a1b5cb21304ae9e849f9
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:01:35 +0000

    Create code.java

---
+++ 题库/试卷2/1/code.java
@@ -0,0 +1,24 @@
+public class Java_1
+{
+    public static void main(String args[])
+    {
+        int i,count;
+        
+     //*********Found********
+        ________________;
+        
+        for( i=100 ; i <= 200 ; i++)
+     //*********Found********
+            if ( _________________ ) count++;
+        
+     //*********Found********
+        System.out.println("Count = " + ______________);
+    }  
+}
+
+
+
+
+// (1)count=0 
+// (2)i%3==0 
+// (3)count
