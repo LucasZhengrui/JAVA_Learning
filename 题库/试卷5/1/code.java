4ad185391ab609cc08c0cb4b74f7c4bb966ce827
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:28:53 +0000

    Create code.java

---
+++ 题库/试卷5/1/code.java
@@ -0,0 +1,15 @@
+public class Java_1 {
+    public static void main(String[] args) {
+        //*********Found**********
+        for (int i=1;___________;i++){
+            for(int k=1;k<=5-i;k++)
+                //*********Found**********
+                ______________________;
+            //*********Found**********
+            for(___________;j<=2*i-1;j++)
+                System.out.print("*");
+            //*********Found**********
+            ______________________;
+        }
+    }
+}
