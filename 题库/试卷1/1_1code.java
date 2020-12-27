44f06ac82290f4d3b31c196c38b5a737a6a126bc
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 04:50:11 +0000

    Create 1_1code.java

---
+++ 题库/试卷1/1_1code.java
@@ -0,0 +1,21 @@
+public class Java_1
+{
+   //*********Found**********
+   public static void _________________(_________________ args[])
+   {
+      byte  b = 8;
+      long  g = 567L;
+      float f = 3.1415f;
+      double d = 2.789;
+      int ii = 207;
+      //*********Found**********
+      _________________ gg = g+ii;
+      float ff = b*f;
+      double dd = ff/ii+d;
+      System.out.println("ii= "+ii);
+      System.out.println("gg= "+gg);
+      System.out.println("ff= "+ff);
+      System.out.println("dd= "+dd);
+   }
+}
+
