30f8e26ab782cf7189ea0c282f496cf306eb8b0a
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:46:37 +0000

    Create code.java

---
+++ 题库/试卷8/1/code.java
@@ -0,0 +1,27 @@
+// 本题的要求是∶ 
+// 完善程序，使程序输出结果如下∶ 
+// 2 
+// 0.5
+// 【第一空】 参考答案∶main 
+// 【第二空】 参考答案∶ String 
+// 【第三空】 参考答案∶new 
+
+//取模运算符在整数和浮点数中的应用
+public class Java_1
+{
+   void residual()
+   {
+      int i=5,j=3;
+      float m=456.5f,n=8.0f;
+      System.out.println(i%j);
+      System.out.println(m%n);
+   }
+   //*********Found**********
+   public static void ________________ (______________ args[])
+   {
+      //*********Found**********
+      Java_1 OperAndExp= _________________ Java_1();
+      //取模运算符在整数和浮点数中的应用
+      OperAndExp.residual();
+   }
+}
