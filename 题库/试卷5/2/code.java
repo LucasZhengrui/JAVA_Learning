79fc57f2a4a3c75d681cd4beeca0d4e855b35abb
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:31:39 +0000

    Create code.java

---
+++ 题库/试卷5/2/code.java
@@ -0,0 +1,26 @@
+// 该程序的功能是∶创建一个二维整型数组，并将其以4行5列对齐的格式输出。程序运行结果如下∶ 
+// 1 1 1 1 1 
+// 2 2 2 2 2 
+// 3 3 3 3 3 
+// 4 4 4 4 4 
+// 【参考答案】 
+// (1) j<aMatrix[i].length 
+// (2)aMatrix[i][j]+"" 
+
+public class Java_2
+{
+    public static void main(String[] args) {
+        int[][] aMatrix = {{1,1,1,1,1},{2,2,2,2,2},{3,3,3,3,3},{4,4,4,4,4}};
+		int i = 0; //循环变量
+		int j = 0; //循环变量
+        //print matrix
+        for (i = 0; i < aMatrix.length; i++) {
+   //*********Found********
+	       for ( j = 0; __________________ ; j++) {
+   //*********Found********
+	        System.out.print(__________________ + " ");
+	       }
+	    System.out.println();
+        }
+    }
+}
