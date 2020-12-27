aa9a3029bb7f293f694ef49722e710f9355d6591
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:47:34 +0000

    Create code.java

---
+++ 题库/试卷8/2/code.java
@@ -0,0 +1,29 @@
+// 本题的要求是∶ 
+// 创建二维数组并将其元素输出。 
+// 运行结果为∶ 
+// 1 1 1 1 
+// 2 2 2 2 
+// 3 3 3 3 
+// 4 4 4 4 
+// 【第一空】 参考答案∶ aMatrix.length 
+// 【第二空】 参考答案; aMatrix[i][j] 
+
+public class Java_2
+{
+    public static void main(String[] args)
+    {
+        int[][] aMatrix = {{1,1,1,1},{2,2,2,2},{3,3,3,3},{4,4,4,4}};
+        int i = 0; //循环变量
+        int j = 0; //循环变量
+     //*********Found********
+        for (i = 0; i < ________________; i++)
+        {
+            for (j = 0; j < aMatrix[i].length; j++ )
+            {
+     //*********Found********
+                System.out.print(________________ + " ");
+            }
+            System.out.println();
+        }
+    }
+}
