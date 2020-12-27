8ae3bf808d49bf003833e3ab3fa5197f033eaa75
LucasZhengrui <52554274+LucasZhengrui@users.noreply.github.com>
2020-12-27 05:17:41 +0000

    Update code.java

--- 题库/试卷2/2/code.java
+++ 题库/试卷2/2/code.java
@@ -1,3 +1,14 @@
+// 本题的要求是∶ 
+// 请阅读Java_2.java 应用实例，完成程序并运行得结果为∶ 
+// 1 
+// 1 2 
+// 1 2 3 
+// 1 2 3 4 
+// 1 2 3 4 5 
+// 1 2 3 4 5 6 
+// 【第一空】 参考答案∶int[6] 
+// 【第二空】 参考答案∶ aMatrix.length或 6 
+
 public class Java_2 {
    public static void main(String[] args) {
      //*********Found********
